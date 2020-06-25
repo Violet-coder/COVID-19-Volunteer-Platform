fd7eb42dcc55ac2afe054ce071e9971d8ecdc01b
Ooalh <zhangyifansugar@gmail.com>
2020-06-25 16:04:23 -0400

    footer

--- src/react-components/Footer/index.js
+++ src/react-components/Footer/index.js
@@ -26,7 +26,7 @@ class Footer extends React.Component {
           <div>
         <div className="row">
           <div className="col-md-8 col-sm-6 col-xs-12">
-            <p id="intro">Copyright &copy; 2020 All Rights Reserved by  
+            <p id="copyright">Copyright &copy; 2020 All Rights Reserved by  
          <a href="#"> CSC309/Team20</a>.
             </p>
           </div>

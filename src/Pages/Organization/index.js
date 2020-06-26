7287a7399bdc67bbc61aca3ae2df724b453267e6
Ooalh <zhangyifansugar@gmail.com>
2020-06-26 16:49:11 -0400

    add volusers

--- src/Pages/Organization/index.js
+++ src/Pages/Organization/index.js
@@ -10,13 +10,13 @@ class Organization extends React.Component{
     state = {
       posts: [
         { name: 'Driver', description: "Deliver food", requirement: "driver's license, multi-task", title: "Driver", status: "Approved", date: "6/15/2020", location: "Toronto", id: 1},
-        { name: "Rider", description: "Deliver food", requirement: "self-motivated, repititive task, self-motivated, repititive task, self-motivated", title: "Driver", status: "Approved", date: "6/16/2020", location: "Vancouver", id: 2}
+        { name: "Rider", description: "Deliver food", requirement: "self-motivated, repititive task", title: "Driver", status: "Approved", date: "6/16/2020", location: "Vancouver", id: 2}
       ],
       applicants: [
         {name: 'Jonh Smith', jobId: 1, rank: 'A', status: 'pending', id: 1, jobName: 'Driver'},
         {name: 'Maria Hernandz', jobId: 2, rank: 'B', status: 'pending', id: 2, jobName: 'Rider'},
-        {name: 'Lily', jobId: 2, rank: 'A', status: 'accepted', id: 3, jobName: 'Rider'},
-        {name: 'Lucy', jobId: 1, rank: 'C', status: 'rejected', id: 4, jobName: 'Driver'},
+        {name: 'Hannah Logan', jobId: 2, rank: 'A', status: 'accepted', id: 3, jobName: 'Rider'},
+        {name: 'Janice Bingham', jobId: 1, rank: 'C', status: 'rejected', id: 4, jobName: 'Driver'},
       ],
       info: {
         name: "Listening Society",

ffe91d09a8e85243b5006a2451b96a1f1b48c264
Ooalh <zhangyifansugar@gmail.com>
2020-08-04 10:38:35 +0800

    db

--- models/organization.js
+++ models/organization.js
@@ -1,6 +1,8 @@
+'use strict';
 const mongoose = require('mongoose')
-
-const Organization = mongoose.model('Organization', {
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const OrganizationSchema = new mongoose.Schema({
 	name: {
 		type: String,
 		required: true,
@@ -8,16 +10,73 @@ const Organization = mongoose.model('Organization', {
 		trim: true
     },
     email: {
-        type: String,
-		required: true,
-		minlegth: 1,
-		trim: true
-    },
-	introduction: {
 		type: String,
 		required: true,
-		// default: 1
+		minlength: 1,
+		trim: true,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,   // custom validator
+			message: 'Not valid email'
+		}
+	}, 
+	password: {
+		type: String,
+		required: true,
+		minlength: 6
+    },
+    website: {
+		type: String,
+        required: false,
+        trim: true
+	},
+	introduction: {
+		type: String,
+        required: false,
+        trim: true
+	}
+})
+OrganizationSchema.pre('save', function(next) {
+	const user = this; // binds this to User document instance
+
+	// checks to ensure we don't hash password more than once
+	if (user.isModified('password')) {
+		// generate salt and hash the password
+		bcrypt.genSalt(10, (err, salt) => {
+			bcrypt.hash(user.password, salt, (err, hash) => {
+				user.password = hash
+				next()
+			})
+		})
+	} else {
+		next()
 	}
 })
 
+// A static method on the document model.
+// Allows us to find a User document by comparing the hashed password
+//  to a given one, for example when logging in.
+OrganizationSchema.statics.findByEmailPassword = function(email, password) {
+	const User = this // binds this to the User model
+
+	// First find the user by their email
+	return User.findOne({ email: email }).then((user) => {
+		if (!user) {
+			return Promise.reject()  // a rejected promise
+		}
+		// if the user exists, make sure their password is correct
+		return new Promise((resolve, reject) => {
+			bcrypt.compare(password, user.password, (err, result) => {
+				if (result) {
+					resolve(user)
+				} else {
+					reject()
+				}
+			})
+		})
+	})
+}
+
+// make a model using the User schema
+const Organization = mongoose.model('Oranization', OrganizationSchema)
 module.exports = { Organization }
---
+++ models/post.js
@@ -0,0 +1,35 @@
+'use strict';
+const mongoose = require('mongoose')
+
+const Post = mongoose.model('Post', {
+	name: {
+		type: String,
+		required: true,
+		minlegth: 1,
+		trim: true
+    },
+    description: {
+		type: String,
+		required: true,
+        minlength: 1,
+        trim: true
+	}, 
+	relevant_area: {
+		type: String,
+		required: true,
+    },
+    location: {
+		type: String,
+		required: true,
+	},
+	requirements: {
+		type: String,
+		required: false,
+    },
+    is_approved: {
+        type: Boolean,
+        required: true
+    }
+})
+
+module.exports = { Post }

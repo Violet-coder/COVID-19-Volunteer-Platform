6ffa3faf9a2b288e27ea0e229e288c1f6ffd4108
Ooalh <zhangyifansugar@gmail.com>
2020-08-08 06:52:24 +0800

    Update server.js

--- server.js
+++ server.js
@@ -460,7 +460,7 @@ app.post("/organization", (req, res) => {
     const organization = new Organization({
         email: req.body.email,
         password: req.body.password,
-        name: req.body.firstname,
+        name: req.body.name,
         posts: []
     });
 
